46549024056b87f577d280c19905bbf78db9187e
Roberto Oliveira <roliveira.sagan@gmail.com>
2026-03-13 20:14:36 -0300

    V.2

--- main.tf
+++ main.tf
@@ -27,13 +27,13 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = "iac-lab"
-  user_data_replace_on_change = true
-  user_data = <<-EOF
-    #!/bin/bash
-    cd /home/ubuntu
-    echo "<h1>Feito com Terraform</h1>" > index.html
-    nohup busybox httpd -f -p 8080 &
-  EOF
+#  user_data_replace_on_change = true
+# user_data = <<-EOF
+#    #!/bin/bash
+#    cd /home/ubuntu
+#    echo "<h1>Feito com Terraform</h1>" > index.html
+#    nohup busybox httpd -f -p 8080 &
+#  EOF
   tags = {
     Name = "learn-terraform"
   }
